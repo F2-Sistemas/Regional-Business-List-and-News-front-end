0cebabd6150e528eca54e899589aaeb066ea2598
Tiago França <tiago@tiagofranca.com>
2022-09-19 16:53:22 -0300

    On branch wip/gh-pages
    	modified:   scripts/install-dependencies.sh

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -12,10 +12,13 @@ fi
 which php >/dev/null 2>&1;
 
 if [ $? -ne 0 ]; then
-    sudo apt install php-cli -y
+    sudo apt install php php-cli -y
 fi
 
 ## Check and install packages dependencies
+
+export PNPM_HOME="${HOME}/.local/share/pnpm"
+export PATH="$PNPM_HOME:$PATH"
 
 ## pnpm global install
 which pnpm >/dev/null 2>&1;
@@ -31,5 +34,6 @@ fi
 which dot-json >/dev/null 2>&1;
 
 if [ $? -ne 0 ]; then
+    pnpm setup
     pnpm add --global dot-json
 fi
